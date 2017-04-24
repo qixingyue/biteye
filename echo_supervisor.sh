c80f77cb0a2240ba328dab0129d0150f8e72ec09
xingyue <qixingyue@126.com>
2017-04-24 15:19:09 +0800

    supervisor 方式启动

--- echo_supervisor.sh
+++ echo_supervisor.sh
@@ -3,6 +3,7 @@
 p=$(pwd)
 
 echo "
+
 [program:biteye]
 command=python -u ${p}/api/api.py
 user = root
@@ -11,4 +12,15 @@ autorestart = true
 startsecs = 10
 stderr_logfile = ${p}/logs/api_stderr.log
 stdout_logfile=${p}/logs/api_stdout.log
+
+[program:page]
+command=python -m SimpleHTTPServer 
+user = root 
+autostart = true
+autorestart = true
+startsecs = 10
+stderr_logfile = ${p}/logs/page_stderr.log
+stdout_logfile=${p}/logs/page_stdout.log
+directory=${p}/html/
+
 "
