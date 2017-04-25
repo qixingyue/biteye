d1a236413e69c151bd768080ef093f4ceab299a6
xingyue <qixingyue@126.com>
2017-04-25 11:14:47 +0800

    说明扩展点

--- html/static/js/hook.js
+++ html/static/js/hook.js
@@ -2,10 +2,11 @@
 
 /*
  * other code you can use hook function like this :
- * hook.filter(<hook_name>,a,b,c...)
- * then hook.ext[hook_name] will be called if it is defined 
- * hook ext can be defined in other files default in the hooks folder , 
- * by usejs in _init function , that can be brought in .
+ * <hooks>/<htmlname>.js will be loaed even not exits .
+ *
+ * useful hooks :
+ * hook.ext.control  define extension to control for a single page.
+ *
  */
 var hook = {
 
