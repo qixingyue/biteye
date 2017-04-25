9bc56631fd6ace8906a6ac473374d8849a64ae10
xingyue <qixingyue@126.com>
2017-04-25 19:48:04 +0800

    blank.html

--- html/static/js/hooks/blank.js
+++ html/static/js/hooks/blank.js
@@ -8,7 +8,7 @@ hook.ext.hi = function(a,b,c){
 hook.ext.control = function(c){
 
     c.hi_blank = function(){
-        c.jsecho("<h4>Hi , traced from hook function !</h4>");
+        c.jsecho("<h4 style='color:gray;margin:50px;'>Hi , traced from hook function !</h4>");
     }
 
     return c;
