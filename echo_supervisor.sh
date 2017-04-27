ff6e75dd4a8cf4fcada7cd8b392722c84dcb4d18
xingyue <qixingyue@126.com>
2017-04-27 16:10:24 +0800

    输出格式微调

--- echo_supervisor.sh
+++ echo_supervisor.sh
@@ -5,22 +5,22 @@ p=$(pwd)
 echo "
 
 [program:biteye]
-command=python -u ${p}/api/api.py
+command = python -u ${p}/api/api.py
 user = root
 autostart = true
 autorestart = true
 startsecs = 10
 stderr_logfile = ${p}/logs/api_stderr.log
-stdout_logfile=${p}/logs/api_stdout.log
+stdout_logfile = ${p}/logs/api_stdout.log
 
 [program:page]
-command=python -m SimpleHTTPServer 
+command = python -m SimpleHTTPServer 
 user = root 
 autostart = true
 autorestart = true
 startsecs = 10
 stderr_logfile = ${p}/logs/page_stderr.log
-stdout_logfile=${p}/logs/page_stdout.log
-directory=${p}/html/
+stdout_logfile = ${p}/logs/page_stdout.log
+directory = ${p}/html/
 
 "
